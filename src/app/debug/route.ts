4e726feb6b34e0b463cb58ba44ffb910b5f53d53
Reza Yusuf Maulana <reza.yusuf98@gmail.com>
2026-07-11 03:06:20 +0700

    chore: remove debug route

--- src/app/debug/route.ts
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server'
-
-export async function GET() {
-  return NextResponse.json({
-    hasToken: !!process.env.BLOB_READ_WRITE_TOKEN,
-    tokenPrefix: process.env.BLOB_READ_WRITE_TOKEN ? process.env.BLOB_READ_WRITE_TOKEN.substring(0, 5) : null,
-    vercelEnv: process.env.VERCEL_ENV,
-  })
-}
