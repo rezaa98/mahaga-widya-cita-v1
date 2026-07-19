9c0f380f67175080b966023432d906c95b4671ae
Reza Yusuf Maulana <reza.yusuf98@gmail.com>
2026-07-19 23:42:38 +0700

    fix: remove orphaned migrations index

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20260719_164017 from './20260719_164017';
-
-export const migrations = [
-  {
-    up: migration_20260719_164017.up,
-    down: migration_20260719_164017.down,
-    name: '20260719_164017'
-  },
-];
